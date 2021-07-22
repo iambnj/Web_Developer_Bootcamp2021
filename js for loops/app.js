abbae6eb7ec8a7fb346f1fc6fd8487911ebeee9a
Brandon Johnson <yungdun512@gmail.com>
2021-07-22 03:15:41 -0500

    Creating complex For loops

--- js for loops/app.js
+++ js for loops/app.js
@@ -1,4 +1,27 @@
+for (let n = 1; n <= 12; n++) {
+    console.log(n);
+}
+
+for (let odd = 1; odd <= 20; odd+=2) {
+    console.log(odd)
+}
+
+for (let even = 2; even <= 20; even+=2) {
+    console.log(even)
+}
+
 for (let i = 1; i <= 12; i++) {
     console.log(i);
 }
 
+for (let x = 100; x >= 0; x--) {
+    console.log(x);
+}
+
+for (let y = 100; y >= 0; y-=10) {
+    console.log(y);
+}
+
+for (let m=10; m<=1000; m*=10) {
+    console.log('Your answer is: ', m);
+}
