ce36e2ef8c49ba43b95cc321798788aa58d54b6c
Brandon Johnson <yungdun512@gmail.com>
2021-07-27 03:51:12 -0500

    learning for...of loops and basic functions

---
+++ js functions/app.js
@@ -0,0 +1,11 @@
+function grumpus () {
+    console.log('ugh...you again...');
+    console.log('for the last time...');
+    console.log('LEAVE ME ALONE!!!');
+}
+
+grumpus()
+grumpus()
+grumpus()
+grumpus()
+grumpus()
